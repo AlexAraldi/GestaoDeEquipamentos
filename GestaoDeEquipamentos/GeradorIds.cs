659a4be38eed3fdcbd66951aaafec65e04bc8bd5
AlexAraldi <alexanderaraldi@gmail.com>
2025-04-04 15:48:08 -0300

    Implementado primeira parte da aula com POO

---
+++ GestaoDeEquipamentos/GeradorIds.cs
@@ -0,0 +1,21 @@
+﻿namespace GestaoDeEquipamentos.ConsoleApp
+{
+    internal partial class Program
+    {
+        public static class GeradorIds
+        {
+            public static int IdEquipamentos = 0;
+
+            public static int GerarIdEquipamento()
+            {
+                IdEquipamentos++;
+
+                return IdEquipamentos;
+            }
+
+            public static Guid GerarGuidEquipamento()
+            {
+                return Guid.NewGuid();
+            }
+        }
+}
--- GestaoDeEquipamentos/Program.cs
+++ GestaoDeEquipamentos/Program.cs
@@ -1,10 +1,43 @@
-﻿namespace GestaoDeEquipamentos
+﻿using static GestaoDeEquipamentos.Program;
+
+namespace GestaoDeEquipamentos.ConsoleApp
 {
-    internal class Program
+    internal partial class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            TelaEquipamento telaEquipamento = new TelaEquipamento();
+
+            while (true)
+            {
+                string opcaoEscolhida = telaEquipamento.ApresentarMenu();
+
+                switch (opcaoEscolhida)
+                {
+                    case "1":
+                        telaEquipamento.CadastrarEquipamento();
+                        break;
+
+                    case "2":
+                        telaEquipamento.EditarEquipamento();
+                        break;
+
+                    case "3":
+                        telaEquipamento.ExcluirEquipamento();
+                        break;
+
+                    case "4":
+                        telaEquipamento.VisualizarEquipamentos(true);
+                        break;
+
+                    default:
+                        Console.WriteLine("Saindo do programa...");
+                        break;
+                }
+
+                Console.ReadLine();
+            }
+
         }
     }
 }
